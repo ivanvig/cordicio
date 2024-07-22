25e701ebd07e666344abce385e5abfa9a4289737
ivanvig <ivanmvig@gmail.com>
2024-07-21 22:43:51 -0300

    relaxed test error requirement and increased number of iterations

--- src/test/scala/CordicSpec.scala
+++ src/test/scala/CordicSpec.scala
@@ -18,7 +18,7 @@ class CordicTest extends AnyFlatSpec with ChiselScalatestTester with Matchers{
   val cordic_gain = 1.6467605
   behavior of "Cordic"
   it should "generate a sin and cos wave" in {
-    test(new IterativeCordic(16, 10, 15)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
+    test(new IterativeCordic(16, 10, 20)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
       val phi = DenseVector.tabulate(20){x => 2.0*Pi*x/20}
 
       dut.io.in_start.poke(true.B)
@@ -42,14 +42,14 @@ class CordicTest extends AnyFlatSpec with ChiselScalatestTester with Matchers{
 
         while( dut.io.out_busy.peekBoolean() ) { dut.clock.step() }
 
-        dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (cos(ang) +- 0.005)
-        dut.io.out_y.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (sin(ang) +- 0.005)
+        dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (cos(ang) +- 0.01)
+        dut.io.out_y.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (sin(ang) +- 0.01)
       }
     }
   }
 
   it should "rotate a vector" in {
-    test(new IterativeCordic(16, 10, 10)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
+    test(new IterativeCordic(16, 10, 20)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
       val num = 1+1*i
       val ang = -Pi/2
 
@@ -64,13 +64,13 @@ class CordicTest extends AnyFlatSpec with ChiselScalatestTester with Matchers{
 
       while( dut.io.out_busy.peekBoolean() ) { dut.clock.step() }
 
-      dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (1.0 +- 0.005)
-      dut.io.out_y.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (-1.0 +- 0.005)
+      dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (1.0 +- 0.01)
+      dut.io.out_y.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (-1.0 +- 0.01)
     }
   }
 
   it should "convert from polar to cartesian" in {
-    test(new IterativeCordic(16, 10, 10)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
+    test(new IterativeCordic(16, 10, 20)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
       1 to 10 foreach {_ => 
         val mag = scala.util.Random.between(0, 1.5)
         val ang = scala.util.Random.between(0, 2*Pi)
@@ -94,15 +94,15 @@ class CordicTest extends AnyFlatSpec with ChiselScalatestTester with Matchers{
 
         while( dut.io.out_busy.peekBoolean() ) { dut.clock.step() }
 
-        dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (expected.real +- 0.005)
-        dut.io.out_y.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (expected.imag +- 0.005)
+        dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (expected.real +- 0.01)
+        dut.io.out_y.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (expected.imag +- 0.01)
 
       }
     }
   }     
 
   it should "calculate arctan function" in {
-    test(new IterativeCordic(16, 10, 10)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
+    test(new IterativeCordic(16, 10, 20)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
       1 to 10 foreach {_ => 
         val num = scala.util.Random.between(-1.5, 1.5)
 
@@ -117,13 +117,13 @@ class CordicTest extends AnyFlatSpec with ChiselScalatestTester with Matchers{
 
         while( dut.io.out_busy.peekBoolean() ) { dut.clock.step() }
 
-        ((dut.io.out_z.peekInt().toDouble*2*Pi)/(pow(2,16))) should be (atan(num) +- 0.005)
+        ((dut.io.out_z.peekInt().toDouble*2*Pi)/(pow(2,16))) should be (atan(num) +- 0.01)
 
       }
     }
   }     
   it should "convert from cartesian to polar" in {
-    test(new IterativeCordic(16, 10, 10)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
+    test(new IterativeCordic(16, 10, 20)).withAnnotations(Seq(WriteVcdAnnotation)) { dut => 
       1 to 10 foreach { _ => 
         val expected_mag = scala.util.Random.between(0, 1.5)
         val expected_ang = scala.util.Random.between(-Pi, Pi)
@@ -140,8 +140,8 @@ class CordicTest extends AnyFlatSpec with ChiselScalatestTester with Matchers{
 
         while( dut.io.out_busy.peekBoolean() ) { dut.clock.step() }
 
-        dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (expected_mag +- 0.005)
-        (2*Pi*dut.io.out_z.peekInt().toDouble/pow(2,16)) should be (expected_ang +- 0.005)
+        dut.io.out_x.peekInt().toDouble/(cordic_gain*pow(2,10)) should be (expected_mag +- 0.01)
+        (2*Pi*dut.io.out_z.peekInt().toDouble/pow(2,16)) should be (expected_ang +- 0.01)
       }
     }
   }     
